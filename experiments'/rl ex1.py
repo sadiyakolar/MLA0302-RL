7ed679753b554d0a18b09065159cddf6632bb187
sadiyakolar <sadiyakolar786@gmail.com>
2026-08-04 08:20:43 +0530

    Refactor robot cleaning simulation with user input
    Refactor robot cleaning simulation to allow user-defined grid size, start position, dirt, and obstacle cells. Implement random and greedy policies for cleaning.

--- experiments'/rl ex1.py
+++ experiments'/rl ex1.py
@@ -1,33 +1,87 @@
 import random
-SIZE = 5
-dirt = [(1, 2), (2, 4), (4, 1)]
-obstacles = [(1, 1), (3, 3)]
-position = [0, 0]
-reward = 0
-actions = ["UP", "DOWN", "LEFT", "RIGHT"]
-print("Robot Starting Position:", tuple(position))
-for step in range(20):
-    action = random.choice(actions)
-    new_pos = position.copy()
-    if action == "UP" and position[0] > 0:
-        new_pos[0] -= 1
-    elif action == "DOWN" and position[0] < SIZE - 1:
-        new_pos[0] += 1
-    elif action == "LEFT" and position[1] > 0:
-        new_pos[1] -= 1
-    elif action == "RIGHT" and position[1] < SIZE - 1:
-        new_pos[1] += 1
-    position = new_pos
-    if tuple(position) in dirt:
-        reward += 1
-        print(position, "-> Dirt Cleaned (+1)")
-        dirt.remove(tuple(position))
-    elif tuple(position) in obstacles:
-        reward -= 1
-        print(position, "-> Obstacle (-1)")
-    else:
-        print(position, "-> Empty Cell")
-    if len(dirt) == 0:
-        print("All dirt cleaned!")
-        break
-print("Total Reward =", reward)
+SIZE = int(input("Enter Grid Size: "))
+print("Enter Robot Start Position")
+sx = int(input("Row: "))
+sy = int(input("Column: "))
+start = (sx, sy)
+dirt = set()
+n = int(input("Enter Number of Dirt Cells: "))
+print("Enter Dirt Cell Coordinates:")
+for i in range(n):
+    x, y = map(int, input().split())
+    dirt.add((x, y))
+obstacles = set()
+m = int(input("Enter Number of Obstacle Cells: "))
+print("Enter Obstacle Cell Coordinates:")
+for i in range(m):
+    x, y = map(int, input().split())
+    obstacles.add((x, y))
+moves = {
+    "UP": (-1, 0),
+    "DOWN": (1, 0),
+    "LEFT": (0, -1),
+    "RIGHT": (0, 1)
+}
+def valid(x, y):
+    return 0 <= x < SIZE and 0 <= y < SIZE
+def random_policy():
+    position = start
+    dirt_left = set(dirt)
+    reward = 0
+    steps = 0
+    while dirt_left and steps < 50:
+        action = random.choice(list(moves.keys()))
+        dx, dy = moves[action]
+        nx = position[0] + dx
+        ny = position[1] + dy
+        if valid(nx, ny):
+            position = (nx, ny)
+            if position in obstacles:
+                reward -= 1
+                print(position, "Obstacle (-1)")
+            elif position in dirt_left:
+                reward += 1
+                dirt_left.remove(position)
+                print(position, "Dirt Cleaned (+1)")
+            else:
+                print(position, "Empty Cell")
+        steps += 1
+    print("Total Reward =", reward)
+def greedy_policy():
+    position = start
+    dirt_left = set(dirt)
+    reward = 0
+    print("\n===== GREEDY POLICY =====")
+    while dirt_left:
+        target = min(
+            dirt_left,
+            key=lambda d: abs(position[0] - d[0]) + abs(position[1] - d[1])
+        )
+        if position[0] < target[0]:
+            next_pos = (position[0] + 1, position[1])
+        elif position[0] > target[0]:
+            next_pos = (position[0] - 1, position[1])
+        elif position[1] < target[1]:
+            next_pos = (position[0], position[1] + 1)
+        elif position[1] > target[1]:
+            next_pos = (position[0], position[1] - 1)
+        else:
+            next_pos = position
+        position = next_pos
+        if position in obstacles:
+            reward -= 1
+            print(position, "Obstacle (-1)")
+        elif position in dirt_left:
+            reward += 1
+            dirt_left.remove(position)
+            print(position, "Dirt Cleaned (+1)")
+        else:
+            print(position, "Moving")
+    print("Total Reward =", reward)
+print("\nAutonomous Cleaning Robot using MDP")
+print("\nGrid Size:", SIZE, "x", SIZE)
+print("Robot Start Position:", start)
+print("Dirt Cells:", dirt)
+print("Obstacle Cells:", obstacles)
+random_policy()
+greedy_policy()
