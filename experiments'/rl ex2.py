e43f208446657f5bea331de9f67dc47c15af158c
sadiyakolar <sadiyakolar786@gmail.com>
2026-08-04 08:23:09 +0530

    Refactor RL code for dynamic input and value function
    Refactored the code to allow dynamic input for grid size, item locations, goal, obstacles, discount factor, and iterations. Updated the value function calculation to accommodate the new input structure.

--- experiments'/rl ex2.py
+++ experiments'/rl ex2.py
@@ -1,25 +1,46 @@
-SIZE = 4
-ITEM_REWARD = 2
-GOAL_REWARD = 5
-OBSTACLE_PENALTY = -2
-item = (1, 2)
-goal = (3, 3)
-obstacle = (2, 1)
-V = [[0 for j in range(SIZE)] for i in range(SIZE)]
-gamma = 0.9
-for iteration in range(10):
-    new_V = [[0 for j in range(SIZE)] for i in range(SIZE)]
+SIZE = int(input("Enter Grid Size (e.g., 5): "))
+num_items = int(input("Enter number of item locations: "))
+ITEMS = []
+for i in range(num_items):
+    x, y = map(int, input(f"Enter Item {i+1} position (row col): ").split())
+    ITEMS.append((x, y))
+goal_x, goal_y = map(int, input("Enter Goal position (row col): ").split())
+GOAL = (goal_x, goal_y)
+num_obstacles = int(input("Enter number of obstacles: "))
+OBSTACLES = []
+for i in range(num_obstacles):
+    x, y = map(int, input(f"Enter Obstacle {i+1} position (row col): ").split())
+    OBSTACLES.append((x, y))
+gamma = float(input("Enter Discount Factor (e.g., 0.9): "))
+iterations = int(input("Enter Number of Iterations: "))
+V = [[0 for _ in range(SIZE)] for _ in range(SIZE)]
+def next_state(i, j):
+    if j < SIZE - 1:
+        return (i, j + 1)
+    elif i < SIZE - 1:
+        return (i + 1, j)
+    else:
+        return (i, j)
+def reward(state):
+    if state in ITEMS:
+        return 2
+    elif state == GOAL:
+        return 5
+    elif state in OBSTACLES:
+        return -2
+    else:
+        return 0
+for k in range(iterations):
+    new_V = [[0 for _ in range(SIZE)] for _ in range(SIZE)]
     for i in range(SIZE):
         for j in range(SIZE):
-            if (i, j) == goal:
-                new_V[i][j] = GOAL_REWARD
-            elif (i, j) == item:
-                new_V[i][j] = ITEM_REWARD + gamma * V[i][j]
-            elif (i, j) == obstacle:
-                new_V[i][j] = OBSTACLE_PENALTY + gamma * V[i][j]
-            else:
-                new_V[i][j] = gamma * V[i][j]
+            s_next = next_state(i, j)
+            r = reward(s_next)
+            new_V[i][j] = r + gamma * V[s_next[0]][s_next[1]]
     V = new_V
-print("Value Function:")
+print("\nWarehouse Robot Policy Evaluation")
+print("---------------------------------")
 for row in V:
-    print(row)
+    for value in row:
+        print(f"{value:7.2f}", end=" ")
+    print()
