3cb6372a2cc2d1f010445a96fb377960eb911706
sadiyakolar <sadiyakolar786@gmail.com>
2026-08-20 13:47:50 +0530

    Update ex17.py

--- experiments'/ex17.py
+++ experiments'/ex17.py
@@ -1,3 +1,7 @@
+#Set up an environment using OpenAI Gym and implement a policy to solve the MountainCar
+problem. Utilize Python libraries like Keras or TensorFlow to build and train the policy.
+
+
 import gymnasium as gym
 import numpy as np
 import tensorflow as tf
